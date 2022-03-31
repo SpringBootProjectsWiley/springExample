9d711df254d2e18f1de0cd187f44a086e5c500cb
unknown <RohitNaikade1>
2022-03-31 14:15:32 +0530

    first commit

---
+++ src/springExample/com/CRefreshEventHandler.java
@@ -0,0 +1,15 @@
+package springExample.com;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class CRefreshEventHandler implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		
+		System.out.println("Refreshed at :"+event.getTimestamp());
+		
+	}
+
+}
